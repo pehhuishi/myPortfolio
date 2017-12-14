f98983ff2bff871b1116f9d96218573c2386d983
Peh Hui Shi <pehhuishi06@hotmail.com>
2017-12-14 15:38:48 +0800

    Change colours

--- js/init.js
+++ js/init.js
@@ -7,7 +7,6 @@
  jQuery(document).ready(function($) {
 
 
-
 /*----------------------------------------------------*/
 /* FitText Settings
 ------------------------------------------------------ */
@@ -119,48 +118,10 @@
     		$.magnificPopup.close();
     });
 
-/*----------------------------------------------------*/
-/*	contact form
-------------------------------------------------------*/
 
-   $('form#contactForm button.submit').click(function() {
-
-      $('#image-loader').fadeIn();
-
-      var contactName = $('#contactForm #contactName').val();
-      var contactEmail = $('#contactForm #contactEmail').val();
-      var contactSubject = $('#contactForm #contactSubject').val();
-      var contactMessage = $('#contactForm #contactMessage').val();
-
-      var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
-               '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
-
-      $.ajax({
-
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
-
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
-
-	      }
-
-      });
-      return false;
-   });
+   /*----------------------------------------------------*/
+   /*	typeWritter
+   ------------------------------------------------------*/
 
    var TxtType = function(el, toRotate, period) {
         this.toRotate = toRotate;
@@ -218,7 +179,5 @@
         css.innerHTML = ".typeWritter > .wrap { border-right: 0.08em solid #fff}";
         document.body.appendChild(css);
     };
-
-
 
 });
